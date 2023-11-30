c2ed8fddbc1a09bebf37812d7b0b160d7006a827
peogway <duchungbeo2004@example.com>
2023-11-30 23:01:49 +0200

    new

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("xin chasdfo");
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
 serve(handleRequest, { port: 7777 });
