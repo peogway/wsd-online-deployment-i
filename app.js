2e718b80943eb00ba59ef88f3cd1adb4d39d1714
peogway <duchungbeo2004@example.com>
2023-11-30 18:21:01 +0200

    thu

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("He tam bietlldfo you!");
+  return new Response("He tam bietlldfo ybeooou!");
 };
 
 serve(handleRequest, { port: 7777 });
