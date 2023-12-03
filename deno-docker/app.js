9e7e93aed050e8edec16fac99a33420b2a3476bd
peogway <duchungbeo2004@example.com>
2023-12-03 03:35:31 +0200

    text

--- deno-docker/app.js
+++ deno-docker/app.js
@@ -1,12 +1,7 @@
 import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 
-let count = 0;
-
 const handleRequest = (request) => {
-  count++;
-  const content = `<html><head></head><body><h1>${count}</h1></body></html>`;
-
-  return new Response(content);
+  return new Response("Mehsadfasdh42");
 };
 
 serve(handleRequest, { port: 7777 });
