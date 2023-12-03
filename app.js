6cd447c9044ce0c8c3b4ea77ab3b3f9fd643707c
peogway <duchungbeo2004@example.com>
2023-12-03 03:47:14 +0200

    fix

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
 
-let count = 0;
+let count = 5;
 
 const handleRequest = (request) => {
   count++;
   const content = `<html><head></head><body><h1>${count}</h1></body></html>`;
 
-  return new Response(content);
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/html;charset=UTF-8",
+    },
+  });
 };
 
 serve(handleRequest, { port: 7777 });
