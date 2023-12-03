3f2e1db70a098ee64428b07e3e4de7b49c497145
peogway <152217542+peogway@users.noreply.github.com>
2023-12-03 04:40:17 +0200

    Delete deno-docker directory

--- deno-docker/app.js
+++
@@ -1,7 +0,0 @@
-import { serve } from "https://deno.land/std@0.202.0/http/server.ts";
-
-const handleRequest = (request) => {
-  return new Response("Mehsadfasdh42");
-};
-
-serve(handleRequest, { port: 7777 });
