7435a65e7f81507671c1d2f67eb72c33e963bdc9
peogway <duchungbeo2004@example.com>
2023-12-05 05:41:40 +0200

    .

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const handleRequest = async (request) => {
   const url = new URL(request.url);
   if (url.pathname === "/visits") {
     data.count++;
-    return new Response(await renderFile("count.eta", data));
+    return new Response(await renderFile("count.eta", data), responseDetails);
   };
   if (url.pathname === "/meaning"){
     return new Response("Seeking truths beyond meaning of life, you will find 43.");
