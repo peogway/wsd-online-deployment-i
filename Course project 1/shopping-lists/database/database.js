c229807c518b726b4270f3ccde90b91696b261a7
peogway <152217542+peogway@users.noreply.github.com>
2023-12-20 02:37:27 +0200

    Update database.js

--- Course project 1/shopping-lists/database/database.js
+++ Course project 1/shopping-lists/database/database.js
@@ -4,13 +4,7 @@ let sql;
 if (Deno.env.get("DATABASE_URL")) {
   sql = postgres(Deno.env.get("DATABASE_URL"));
 } else {
-  sql = postgres({
-  host: "flora.db.elephantsql.com",
-  database: "legjwppq",
-  username: "legjwppq",
-  password: "N7MZclEmfb7boNtT3THiMzJFNGZl1AF7",
-  port: 5432,
-  });
+  sql = postgres({});
 }
 
 export { sql };
