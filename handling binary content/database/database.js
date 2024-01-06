3f6eccc7859f48628db66366f5989e6a612e9e56
peogway <duchungbeo2004@example.com>
2024-01-06 23:37:51 +0200

    new

--- handling binary content/database/database.js
+++ handling binary content/database/database.js
@@ -1,9 +1,6 @@
 import postgres from "https://deno.land/x/postgresjs@v3.4.2/mod.js";
 
-let sql;
-if (Deno.env.get("DATABASE_URL")) {
-  sql = postgres(Deno.env.get("DATABASE_URL"));
-} else {
+
   sql = postgres({
     host: "flora.db.elephantsql.com",
     database: "legjwppq",
@@ -11,6 +8,6 @@ if (Deno.env.get("DATABASE_URL")) {
     password: "N7MZclEmfb7boNtT3THiMzJFNGZl1AF7",
     port: 5432,
   });
-}
+
 
 export { sql };
